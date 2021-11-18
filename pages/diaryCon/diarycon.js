e611b6b86cf639c3d3026d8bfaa5ebfdd9e6b410
muyu9 <unknown@example.com>
2021-11-18 19:11:47 +0800

    更新日记详情页面

--- pages/diaryCon/diarycon.js
+++ pages/diaryCon/diarycon.js
@@ -2,7 +2,7 @@ const date = new Date()
 const years = []
 const months = []
 const days = []
-
+var app = getApp()
 for (let i = 1990; i <= date.getFullYear(); i++) {
   years.push(i)
 }
@@ -20,64 +20,49 @@ Page({
    * 页面的初始数据
    */
   data: {
+    idkey:-1,
     scrollTop:0,
-      "content":"你好呀哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈或或或或爱我中华爱我中华爱我中华",
-       value: [days[date.getDate()-1],months[date.getMonth()], date.getFullYear()]
-  },
+    content:"",
+    mood:"",
+    day:"",
+    month:"",
+    year:"",
+  }, 
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-    
+    var idkey=options.idkey;
+    console.log(options.idkey);
+    this.setData({
+      idkey:idkey,
+      mood:wx.getStorageSync("d_mood"),
+      content:wx.getStorageSync("d_content"),
+      day:wx.getStorageSync("d_day"),
+      month:wx.getStorageSync("d_month"),
+      year:wx.getStorageSync("d_year"),
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  deleteDia:function(){
+    var that=this;
+    // wx.request({
+    //   url: 'https://luckym.top/diary/deleteDiary/',
+    //   method: 'post',
+    //   header:{
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   data: {
+    //     diary_id:that.data.idkey,
+    //   },
+    //   success:function(res){
+    //     // that.data.content=res.data.content;
+    //     console.log(res.data)
+    //   },
+    //   fail:function(res){
+    //     console.log("失败")
+    //   }
+    // })
+    app.globalData.deleteDiary=true;
+    wx.navigateBack({
+      delta: 1,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
